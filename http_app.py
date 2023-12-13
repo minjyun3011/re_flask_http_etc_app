e5bf1814b250fccac06818eeca818a552e06164d
minjyun3011 <grand3canyon10@gmail.com>
2023-12-13 15:14:31 +0900

    ログインの基礎機能完成

---
+++ http_app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return '''
+        <form action="/login" method="post">
+            Password:<input type="text"><br>
+            <input type='submit'>
+        </form>
+        '''
+    return "Logged in!"
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
